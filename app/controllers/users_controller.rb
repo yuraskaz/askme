ae351a546b6735b683f11c20593c59e99387d269
yuraskaz <kazachkoff.yury@yandex.ru>
2017-01-04 22:16:56 +0300

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def show
+    @hello = "Парам пам пам"
+  end
+end
---
+++ app/models/question.rb
@@ -0,0 +1,9 @@
+class Question < ActiveRecord::Base
+
+  belongs_to :user
+
+  validates :text, :user, presence:true
+  #проверка максимальной длины создаваемого вопроса-не более 255
+  #если более 255 символов то вопрос не будет создан
+  validates :text , length:  { maximum: 256}
+end
