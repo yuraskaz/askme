f50ffd14bd5d5ab8243683d5ce8815c8ba0ac794
yuraskaz <kazachkoff.yury@yandex.ru>
2017-01-10 00:02:07 +0300

    'AddIndexUsernameToUser'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hello = "Парам пам пам"
+    @user = User.new(
+                    name:'Yura',username:'yuraskaz',avatar_url:'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
+    )
+    @new_question= Question.new
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102142757) do
+ActiveRecord::Schema.define(version: 20170109205050) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "text"
@@ -33,5 +33,7 @@ ActiveRecord::Schema.define(version: 20170102142757) do
     t.string   "password_salt"
     t.string   "avatar_url"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
