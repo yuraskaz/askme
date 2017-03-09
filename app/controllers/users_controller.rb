213775b4ad51c63fc5eb3b8b3214ae5999ff788f
yuraskaz <kazachkoff.yury@yandex.ru>
2017-03-10 00:13:40 +0300

    'add_count_questions_answers'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,64 @@
 class UsersController < ApplicationController
+
+  before_action :load_user, except: [:index,:new,:create]
+  before_action :authorize_user , except: [:index, :create, :new,:show]
+
   def index
+    @users = User.all
   end
 
   def new
+    redirect_to root_url,alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new
   end
 
   def edit
+
+  end
+
+  def create
+    redirect_to root_url,alert: 'Вы уже залогинены' if current_user.present?
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: 'пользователь успешно зарегестрирован!'
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновленны'
+    else
+      render 'edit'
+    end
+
   end
 
   def show
-    @user = User.new(
-                    name:'Yura',username:'yuraskaz',avatar_url:'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100'
-    )
-    @new_question= Question.new
+
+    @questions = @user.questions.order(created_at: :desc)
+
+    @new_question = @user.questions.build
+    @questions_count = @questions.count
+    @answers_count = @questions.where.not(answer: nil).count
+    @unanswered_count = @questions_count - @answers_count
+
+  end
+  private
+  #фильтр перед каждым action
+  def load_user
+    @user = User.find params[:id]
+  end
+  def authorize_user
+    reject_user unless @user == current_user
+
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,41 @@
+# (c) goodprogrammer.ru
+#
+# В этом файле мы можем писать вспомогательные методы (хелперы)
+# для представлений (view) нашего приложения
 module ApplicationHelper
+  #метод возвращающий в правильном склонении слова(склонятор)
+  def quantity_word(number,words)
+    if number == nil
+      number = 0
+    end
+    ostatok = number % 10
+    if ostatok == 1
+      return words[0]
+    end
+
+    if ostatok >= 2 && ostatok <= 4
+      return words[1]
+    end
+
+    if (ostatok >= 5 && ostatok <= 9) || ostatok == 0
+      return words[2]
+    end
+
+  end
+
+  # Этот метод возвращает ссылку на автарку пользователя, если она у него есть
+  # Или ссылку на дефолтную аватарку, которая лежит в app/assets/images
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'avatar.jpg'
+    end
+  end
+
+
+  # Хелпер, рисующий span тэг с иконкой из font-awesome
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
 end
