d0d90034199f0e9577bd15874873a70054cbb941
yuraskaz <kazachkoff.yury@yandex.ru>
2017-06-02 14:05:04 +0300

    add delete user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,10 @@ class UsersController < ApplicationController
     @unanswered_count = @questions_count - @answers_count
 
   end
+  def destroy
+    @user.destroy
+    redirect_to root_url, notice: 'аккаунт успешно удален!'
+  end
   private
   #фильтр перед каждым action
   def load_user
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-
-
   root 'users#index'
-  resources :users , except: [:destroy]
+  resources :users
   resources :sessions , only: [:new,:create,:destroy]
 
   resource :questions , exept: [:show,:new,:index]
   get 'sign_up' => 'users#new'
   get 'log_out' => 'sessions#destroy'
   get 'log_in' => 'sessions#new'
+  get 'delete_account'=>  'users#destroy'
 end
